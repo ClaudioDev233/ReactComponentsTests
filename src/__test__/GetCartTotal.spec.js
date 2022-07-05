3c425a2858bf1b5f03e844eeb0fef8de34de37f6
Claudio <claudio.junior233@gmail.com>
2022-07-05 01:49:03 -0300

    GetCartTotalRefact

--- src/__test__/GetCartTotal.spec.js
+++ src/__test__/GetCartTotal.spec.js
@@ -13,3 +13,16 @@ test("it should return the correctly total price of an item", () => {
 
   expect(getCartTotal(wishlistData)).toEqual(output);
 });
+
+test("it should sum the sale price of a item if it exists", () => {
+  const wishlistWithSalePrice = [
+    { id: "4027", addedToCart: true, price: 20.0, salePrice: 10.0 },
+    { id: "5301", addedToCart: false, price: 20.0, salePrice: 10.0 },
+    { id: "6711", addedToCart: true, price: 20.0, salePrice: 10.0 },
+    { id: "1244", addedToCart: false, price: 20.0, salePrice: null },
+    { id: "4765", addedToCart: true, price: 20.0, salePrice: null },
+  ];
+
+  const output = 40;
+  expect(getCartTotal(wishlistWithSalePrice)).toEqual(output);
+});
--- src/utils/GetCartTotal.js
+++ src/utils/GetCartTotal.js
@@ -1,8 +1,11 @@
 const getCartTotal = (wishlist) => {
   let totalPrice = 0;
   for (let i = 0; i < wishlist.length; i++) {
-    const { price, addedToCart } = wishlist[i];
-    if (addedToCart) {
+    const { price, addedToCart, salePrice } = wishlist[i];
+    if (addedToCart && salePrice != null) {
+      totalPrice += salePrice;
+    }
+    if (addedToCart && salePrice == null) {
       totalPrice += price;
     }
   }
@@ -15,6 +18,13 @@ const wishlistData = [
   { id: "6711", addedToCart: true, price: 20.0 },
   { id: "1244", addedToCart: false, price: 20.0 },
   { id: "4765", addedToCart: true, price: 20.0 },
+];
+const wishlistWithSalePrice = [
+  { id: "4027", addedToCart: true, price: 20.0, salePrice: 10.0 },
+  { id: "5301", addedToCart: false, price: 20.0, salePrice: 10.0 },
+  { id: "6711", addedToCart: true, price: 20.0, salePrice: 10.0 },
+  { id: "1244", addedToCart: false, price: 20.0, salePrice: null },
+  { id: "4765", addedToCart: true, price: 20.0, salePrice: null },
 ];
 
 export default getCartTotal;
